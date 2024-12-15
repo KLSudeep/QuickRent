f54cf759c0c944b7da6c8f65c43d3279a0db2140
KLSudeep <sudeepkl2003@gmail.com>
2024-12-15 22:53:44 +0530

    Modified NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,49 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="w-full p-4 shadow-md" style={{ backgroundColor: "#8891FD" }}>
+    <nav className="w-full p-4 shadow-md" style={{ backgroundColor: "#0069d9" }}>
       <div className="flex justify-between items-center">
-      <Link 
-  to="/" 
-  className="text-white text-xl font-bold hover:text-blue-500 transition-all duration-300 transform hover:scale-105">
-  QuickRent
-</Link>
+        <Link 
+          to="/" 
+          className="text-white text-3xl font-extrabold hover:text-blue-900 transition-transform duration-300 transform hover:scale-110"
+          style={{ fontFamily: "'Poppins', sans-serif" }}
+        >
+          QuickRent
+        </Link>
 
-        <ul className="flex space-x-6">
-          <li><Link to="/" className="text-white hover:text-black transform hover:scale-150">Home</Link></li>
-          <li><Link to="/features" className="text-white  hover:text-black transform hover:scale-150">Features</Link></li>
-          <li><Link to="/gadgets" className="text-white  hover:text-black transform hover:scale-150">Gadgets</Link></li>
-          <li><Link to="/contact" className="text-white  hover:text-black transform hover:scale-150">Contact</Link></li>          
+        <ul className="flex space-x-8">
+          <li>
+            <Link 
+              to="/" 
+              className="text-white text-lg font-medium hover:text-orange-200 transition-transform duration-300 transform hover:scale-110"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/features" 
+              className="text-white text-lg font-medium hover:text-orange-200 transition-transform duration-300 transform hover:scale-110"
+            >
+              Features
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/gadgets" 
+              className="text-white text-lg font-medium hover:text-orange-200 transition-transform duration-300 transform hover:scale-110"
+            >
+              Gadgets
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/contact" 
+              className="text-white text-lg font-medium hover:text-orange-200 transition-transform duration-300 transform hover:scale-150"
+            >
+              Contact
+            </Link>
+          </li>          
         </ul>
       </div>
     </nav>
